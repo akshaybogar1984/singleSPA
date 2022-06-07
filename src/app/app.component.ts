6c05c79f5a60ed0d137cfddd45db07a26ec3ad8c
akshaybogar1984 <akshayakumar.bogar@bsigroup.com>
2022-06-07 14:38:15 +0100

    07 today checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>{{message}}</div>`,
+  template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
-  message = '';
-
-  constructor(private http: HttpClient) {
-    this.http.get('/api/message')
-      .subscribe((resp: any) => this.message = resp.text);
-  }
+  value = 'World';
 }
